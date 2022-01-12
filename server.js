ee3090185f926df09e762f77e0cf29afe8616dec
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2022-01-12 12:23:05 -0400

    DELETE route added to /users

--- server.js
+++ server.js
@@ -89,5 +89,25 @@ app.route('/users/:id')
 })
 // DELETE
 .delete((req,res) => {
-  // User.findByIdAndDelete()
+  User.findByIdAndDelete(
+    req.params.id,
+    (err, data) => {
+      if(err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if(!data){
+        res.json({
+          success: false,
+          message: "Not found"
+        })
+      } else {
+        res.json({
+          success: true,
+          message: data
+        })
+      }
+    }
+  )
 })
