ff8b6ea4e7c532e103c1383256c6d0cc79906207
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2022-01-12 12:16:59 -0400

    POST and GET routes added to /users

--- server.js
+++ server.js
@@ -4,27 +4,62 @@ const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
 
+const User = require('./models/User');
+mongoose.connect('mongodb://localhost:27017/userData');
+
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
+app.listen(port, () => {
 	console.log(`server is listening on port:${port}`)
 })
 
 // CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
+app.post('/users',(req,res) => {
+  User.create(
+    {
+      name: req.body.newData.name,
+      email: req.body.newData.email,
+      password: req.body.newData.password
+    },
+    (err, data) => {
+      if (err){
+        res.json({success: false, message: err})
+      }else if(!data){
+        res.json({success: false, message: "Not Found"})
+      } else {
+        res.json({success: true, message: data})
+      }
+    }
+  )
 })
 
 app.route('/users/:id')
 // READ
-.get((req,res)=>{
-  // User.findById()
+.get((req,res) => {
+  User.findById(req.params.id, (err, data) => {
+    if(err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if(!data){
+      res.json({
+        success: false,
+        message: "Not found"
+      })
+    } else {
+      res.json({
+        success: true,
+        message: data
+      })
+    }
+  })
 })
 // UPDATE
-.put((req,res)=>{
+.put((req,res) => {
   // User.findByIdAndUpdate()
 })
 // DELETE
-.delete((req,res)=>{
+.delete((req,res) => {
   // User.findByIdAndDelete()
 })
